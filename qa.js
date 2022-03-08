da898f4368ca7ac3d9c81cb751b028b8b1dcb55d
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-03-08 22:52:53 +0000

    commit

--- qa.js
+++ qa.js
@@ -1,18 +1,9 @@
 const questions = document.querySelectorAll('.question');
 
 questions.forEach(function (e) {
-  const btn = question.querySelector('.question-btn');
+  const btn = e.querySelector('.question-btn');
 
   btn.addEventListener('click', function () {
-    question.classList.toggle('show-text');
+    e.classList.toggle('show-text');
   });
 });
-
-// const btns = document.querySelectorAll('.question-btn');
-
-// btns.forEach(function (btn) {
-//   btn.addEventListener('click', function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
-//     question.classList.toggle('show-text');
-//   });
-// });
